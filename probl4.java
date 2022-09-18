17d06eeddd09253aeef92113261f64ee9d0dd280
Alexander Caballero <112838714+AlexUTP@users.noreply.github.com>
2022-09-18 01:28:10 -0500

    change.java

--- probl4.java
+++ probl4.java
@@ -7,19 +7,20 @@ class probl4 {
 
         System.out.println("Ingrese un numero: ");
         num = sc.nextInt();
-        while(num > 0){
+        while(num > 1){
             if(num % 2 == 0){
-                System.out.println(num + " /2");
+                System.out.println(num + " / 2");
                 num = num / 2;
             }
             else if(num % 3 == 0){
-                System.out.println(num + " /3");
+                System.out.println(num + " / 3");
                 num = num / 3;
             }
             else if(num % 5 == 0){
-                System.out.println(num + " /5");
+                System.out.println(num + " / 5");
                 num = num / 5;
             }
         }
+        System.out.println(num);
     }
 }
