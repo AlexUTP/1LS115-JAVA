d6daad4640f153b890d6c46c27d155a21957587e
Alexander Caballero <112838714+AlexUTP@users.noreply.github.com>
2022-09-18 01:06:21 -0500

    java.change

--- probl4.java
+++ probl4.java
@@ -1,4 +1,25 @@
+import java.util.*;
 
 class probl4 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int num;
+
+        System.out.println("Ingrese un numero: ");
+        num = sc.nextInt();
+        while(num > 0){
+            if(num % 2 == 0){
+                num = num / 2;
+                System.out.println(num + " / 2");
+            }
+            else if(num % 3 == 0){
+                num = num / 3;
+                System.out.println(num + " / 3");
+            }
+            else if(num % 5 == 0){
+                num = num / 5;
+                System.out.println(num + " / 5");
+            }
+        }
+    }
 }
--- probl5.java
+++ probl5.java
@@ -3,12 +3,14 @@ import java.util.*;
 class probl5{
     public static void main(String[] args) {
         Scanner sc= new Scanner (System.in);
-        int num, i;
+        int num, rest, div = 10000;
 
         System.out.println("Ingrese un numero de 5 digitos: ");
         num = sc.nextInt();
-        for(i=0; i<5; i++){
-            
+        while(div > 0){
+            rest = num / div;
+            System.out.println(rest);
+            div = div / 10;
         }
     }
 }
