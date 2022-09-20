8360d3f625ba0bbe4ca6f26cbc1713327540e347
Alexander Caballero <112838714+AlexUTP@users.noreply.github.com>
2022-09-20 00:37:09 -0500

    Create probl9.java

---
+++ practica_n1/probl9.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class probl9 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        char caracter; 
+        int ascii;
+        boolean cierto = true;
+        
+        do{
+            System.out.println("Ingrese el caracter que desea convertir a ASCII: ");
+            caracter = sc.next().charAt(0); 
+            ascii = caracter;
+            if(ascii >= 32 && ascii <= 126){
+                System.out.println("El caracter " + caracter + " es el numero " + ascii + " en la tabla");
+                cierto = false;
+            }
+            else{
+                System.out.println("ERROR: El caracter ingresado no es ASCII.");
+            }
+        }while(cierto);
+    }
+}
+
