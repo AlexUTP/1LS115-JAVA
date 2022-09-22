174a5c3cb645ebc6fed5eec3e614df2281d024ae
Alexander Caballero <112838714+AlexUTP@users.noreply.github.com>
2022-09-22 13:28:05 -0500

    change,java

--- practica_n1/probl7.java
+++ practica_n1/probl7.java
@@ -9,35 +9,38 @@ public class probl7 {
         int i, nummay = 0, nummen = 0;
         int [] num = new int[3];
         double prom = 0;
-       
-        for(i = 0; i < 3 ; i++){ //Inicio arreglo para guardar valores
-            System.out.println("Ingrese un numero ["+ (i+1) + "]: ");
-            num [i] = sc.nextInt();
-            prom = prom + num[i];
-        }
-        
-        if(num[0] > num[1] && num[0] > num[2]){ //Condicion para buscar el valor mayor
-            nummay = num[0];
-        }
-        else if(num[1] > num[0] && num[1] > num[2]){
-            nummay = num[1];
-        }
-        else{
-            nummay = num[2];
-        }
 
-        if(num[0] < num[1] && num[0] < num[2]){ //Condicion para buscar el mayor menor
-            nummen = num[0];
+        try {
+            for(i = 0; i < 3 ; i++){ //Inicio arreglo para guardar valores
+                System.out.println("Ingrese un numero ["+ (i+1) + "]: ");
+                num [i] = sc.nextInt();
+                prom = prom + num[i];
+            }
+            
+            if(num[0] > num[1] && num[0] > num[2]){ //Condicion para buscar el valor mayor
+                nummay = num[0];
+            }
+            else if(num[1] > num[0] && num[1] > num[2]){
+                nummay = num[1];
+            }
+            else{
+                nummay = num[2];
+            }
+    
+            if(num[0] < num[1] && num[0] < num[2]){ //Condicion para buscar el mayor menor
+                nummen = num[0];
+            }
+            else if(num[1] < num[0] && num[1] < num[2]){
+                nummen = num[1];
+            }
+            else{
+                nummen = num[2];
+            }
+            System.out.println("El numero mayor es: "+ nummay + " y el numero menor es: " + nummen);
+            System.out.println("El promedio es: " + df.format(prom / 3));
+            sc.close();
+        } catch (Exception e){
+            System.out.println("ERROR: No introduciste un numero.");
         }
-        else if(num[1] < num[0] && num[1] < num[2]){
-            nummen = num[1];
-        }
-        else{
-            nummen = num[2];
-        }
-
-        System.out.println("El numero mayor es: "+ nummay + " y el numero menor es: " + nummen);
-        System.out.println("El promedio es: " + df.format(prom / 3));
-        sc.close();
     }
 }
--- practica_n1/probl8.java
+++ practica_n1/probl8.java
@@ -9,18 +9,22 @@ public class probl8 {
 
         System.out.println("Ingrese un numero binario de 5 digitos: ");
         num = sc.nextInt();
-        while(true){
-            if(num == 0){
-                break;
-            } 
-            else{
-                int temp = num % 10;
-                decimal += temp * Math.pow(2, p);
-                num = num / 10;
-                p++;
+        try {
+            while(true){
+                if(num == 0){
+                    break;
+                } 
+                else{
+                    int temp = num % 10;
+                    decimal += temp * Math.pow(2, p);
+                    num = num / 10;
+                    p++;
+                }
             }
+            System.out.println("El numero binario convertido a decimal es: " + decimal);
+            sc.close();
+        } catch(Exception e){
+            System.out.println("ERROR: No introduciste un numero binario.");
         }
-        System.out.println("El numero binario convertido a decimal es: " + decimal);
-        sc.close();
     }
 }
