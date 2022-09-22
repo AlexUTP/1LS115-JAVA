927e2628c96ec8a9e5f746df4b1305013df05469
Alexander Caballero <112838714+AlexUTP@users.noreply.github.com>
2022-09-22 12:45:39 -0500

    Update probl10.java

--- practica_n1/probl10.java
+++ practica_n1/probl10.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 public class probl10 {
     public static void main(String[] args){
+        //Declaracion de Variables
         Scanner leer  = new Scanner (System.in);
         String varx;
 
-        while(true)
-        {
+        while(true){ //condicicion while
             System.out.println("Ingrese un valor cualquiera" );
             System.out.println("Escriba Salir para cerrar el programa");
-            varx = leer.nextLine(); 
-            if(varx=="salir")
-            {
+            varx = leer.nextLine();
+
+            varx = varx.toLowerCase();
+            if(varx.equals("salir")){
                 break;
             }
         }
+        leer.close();
     }
 }
