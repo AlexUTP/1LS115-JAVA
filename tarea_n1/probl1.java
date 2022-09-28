1529ac1bf1734587a9eeaaebe5a42b6e5f8a8e87
Camilo Yepes <112560489+CamiloYeps@users.noreply.github.com>
2022-09-28 08:52:59 -0500

    probl1.java

--- tarea_n1/probl1.java
+++ tarea_n1/probl1.java
@@ -3,30 +3,29 @@ import java.util.*;
 public class probl1 {
   public static void main(String[] args){
     Scanner entrada = new Scanner(System.in);
+    String palabra;
     char caracter;
     int valorCaracter;
 
-    try {
-      System.out.print("Introduzca un caracter: ");
-      caracter = entrada.next().charAt(0);
-      valorCaracter = caracter;
+    System.out.print("Introduzca un caracter: ");
+    palabra = entrada.next();
 
-      if (valorCaracter >= 48 && valorCaracter <= 57) {
+    if (palabra.length() == 1) {
+      caracter = palabra.charAt(0);//convierte la cadena a caracter
+      valorCaracter = caracter;//convierte el caracter a su valor ascii
+      if (valorCaracter >= 48 && valorCaracter <= 57) {//verificar si es numero
         System.out.println("USTED HA INGRESADO UN NUMERO");
-      }
-      else if (valorCaracter >= 65 && valorCaracter <= 90) {
+      } else if (valorCaracter >= 65 && valorCaracter <= 90) {//verificar si es una letra mayuscula
         System.out.println("USTED HA INGRESADO UNA LETRA MAYUSCULA");
-      }
-      else if (valorCaracter >= 97 && valorCaracter <= 122) {
+      } else if (valorCaracter >= 97 && valorCaracter <= 122) {//verificar si es una letra minuscula
         System.out.println("USTED HA INGRESADO UNA LETRA MINUSCULA");
-      }
-      else {
+      } else {
         System.out.println("VALOR NO RECONOCIDO");
       }
     }
-    catch (InputMismatchException ex) {
-      System.out.println("Debe ingresar una letra ");
+    else {
+      System.out.println("Usted ha ingresado mas de una letra");
     }
-    entrada.close();
-  }
-}
+    entrada.close();//cerrar scanner
+  }//fin main
+}//fin class
