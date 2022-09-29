d8b21cdc5fc93c0a8ef84b1ce5a82bcaa51583f7
Camilo Yepes <112560489+CamiloYeps@users.noreply.github.com>
2022-09-28 19:32:23 -0500

    probl3.java

---
+++ tarea_n1/probl3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class probl3{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int max, num1 = 0, num2 = 1, sum = 1, cont = 1;
+    try {//atrapa el error
+        System.out.print("Introduce un valor numerico: "); 
+        max = sc.nextInt();
+        System.out.println("Su entrada es: " + max);
+        System.out.print("Su salida es: 0 "); 
+        while (cont <= max) {//calcular la serie Fibonacci
+          System.out.print(sum + " ");
+          sum = num1 + num2;
+          num1 = num2;
+          num2 = sum;
+          cont++;
+        }//fin while
+    } catch (Exception e){//muestra el error
+        System.out.print("ERROR: No introduciste un numero."); 
+    }
+    sc.close();//cerrar scanner
+  }//fin main
+}//fin class
